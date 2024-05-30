7924977e106131200c42c37a7d7898c312afe3c7
betancito <jeronimobeta90@gmail.com>
2024-05-30 03:01:59 -0500

    most pages interconected missing user view and contact page

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	'use strict';
+
+	// Form
+
+	var contactForm = function() {
+
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					fname: "required",
+					lname: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					fname: "Please enter your first name",
+					lname: "Please enter your last name",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+			} );
+		}
+	};
+	contactForm();
+
+});
