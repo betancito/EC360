a93a59c3d2de925e964ce3e73f4463729f8d892b
betancito <jeronimobeta90@gmail.com>
2024-12-20 11:49:02 -0500

    feat:finished backend logic to login or register

--- resources/views/livewire/auth/auth.blade.php
+++ resources/views/livewire/auth/auth.blade.php
@@ -5,7 +5,7 @@
 @endsection
 <div class="container d-flex justify-content-center align-items-center vh-100">
     <div class="wrapper">
-        <div class="card-switch">
+        <div class="card-switch" >
             <label class="switch">
                 <input class="toggle" type="checkbox">
                 <span class="slider"></span>
@@ -13,19 +13,33 @@
                 <div class="flip-card__inner">
                     <div class="flip-card__front">
                         <div class="title">Log in</div>
-                        <form action="" class="flip-card__form">
-                            <input type="email" placeholder="Email" name="email" class="flip-card__input">
-                            <input type="password" placeholder="Password" name="password" class="flip-card__input">
-                            <button class="flip-card__btn">Let`s go!</button>
+                        <form action="login" class="flip-card__form" wire:submit.prevent="login">
+                            <input type="email"  class="flip-card__input" id="email"  name="email" wire:model="email">
+                            @error('email') <span class="text-danger">{{ $message }}</span> @enderror
+                            <input type="password" placeholder="Password" name="password" class="flip-card__input" id="password" wire:model="password">
+                            @error('password') <span class="text-danger">{{ $message }}</span> @enderror
+                            <button type="submit" class="flip-card__btn">Let`s go!</button>
                         </form>
+                        @if (session()->has('error'))
+                        <div class="col-12">
+                            <div class="alert alert-danger">
+                                {{ session('error') }}
+                            </div>
+                        </div>
+                        @endif
                     </div>
                     <div class="flip-card__back">
                         <div class="title">Sign up</div>
-                        <form action="" class="flip-card__form">
-                            <input type="name" placeholder="Name" class="flip-card__input">
-                            <input type="email" placeholder="Email" name="email" class="flip-card__input">
-                            <input type="password" placeholder="Password" name="password" class="flip-card__input">
-                            <button class="flip-card__btn">Confirm!</button>
+                        <form action="register" class="flip-card__form" wire:submit.prevent="register">
+                            <input type="name"  class="flip-card__input" name="name" id="name"  wire:model="name">
+                            @error('name') <span class="text-danger">{{ $message }}</span> @enderror
+                            <input type="email"  class="flip-card__input" id="email"  wire:model="email">
+                            @error('email') <span class="text-danger">{{ $message }}</span> @enderror
+                            <input type="password" placeholder="Password" name="password" class="flip-card__input" id="password" wire:model="r_password">
+                            @error('r_password') <span class="text-danger">{{ $message }}</span> @enderror
+                            <input type="text" class="flip-card__input" id="password_confirmation" wire:model="password_confirmation">
+                            @error('password_confirmation') <span class="text-danger">{{ $message }}</span> @enderror
+                            <button type="submit" class="flip-card__btn">Confirm!</button>
                         </form>
                     </div>
                 </div>
